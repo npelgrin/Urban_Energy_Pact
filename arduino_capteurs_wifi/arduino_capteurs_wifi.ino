97077937833ada573eb3750f063c1010048aef6b
npelgrin <noel.pelgrin@ac-clermont.fr>
2023-12-11 21:58:37 +0100

    fichier de départ

---
+++ arduino_capteurs_wifi/arduino_capteurs_wifi.ino
@@ -0,0 +1,25 @@
+//bibliothèques
+
+
+
+
+//variables globales
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+//Fonctions
+
